8f0d9d90519fcac507e0f2298e8c5607fa0454e3
IvanRamosDataTech <ajaxiank@gmail.com>
2025-03-15 14:50:19 -0600

    - Update Indexed schema to include more excel columns - Portafolio indizado con mejor estructura - Updater now inserts snapshots for indexed investments.

--- sqlscripts/create schema.sql
+++ sqlscripts/create schema.sql
@@ -41,20 +41,23 @@ DROP TABLE IF EXISTS public.indexed;
 
 CREATE TABLE public.indexed
 (
-    "Number" smallint NOT NULL,
-    "Name" character varying(70) NOT NULL,
-    "Ticker" character varying(10) NOT NULL,
-    "Shares" real NOT NULL,
-    "AverageUnitCost" real NOT NULL,
-    "CurrentUnitCost" real NOT NULL,
-    "PurchaseValue" real NOT NULL,
-    "MarketValue" real NOT NULL,
-    "ValueDifference" real NOT NULL,
-    "ValueDifference%" real NOT NULL,
-    "CurrentWeight" real NOT NULL,
-    "DesiredWeight" real NOT NULL,
-    "UploadDate" date,
-    PRIMARY KEY ("Number")
+    "Number" character varying(10) NOT NULL,
+    "Name" character varying(70),
+    "Ticker" character varying(10),
+    "Shares" real,
+    "To Buy" real,
+    "Average Unit Cost" real,
+    "Current Unit Cost" real,
+    "Purchased Value" numeric(15,2) NOT NULL,
+    "Market Value" numeric(15,2) NOT NULL,
+    "Potential Market Value" numeric(15,2) NOT NULL,
+    "Balance" numeric(15,2) NOT NULL,
+    "Balance %" real NOT NULL,
+    "Current Weight %" real NOT NULL,
+    "Potential Weight %" real,
+    "Target Weight %" real NOT NULL,
+    "Snapshot ID" integer NOT NULL,
+    "Snapshot Timestamp" Timestamp
 );
 
 ALTER TABLE IF EXISTS public.indexed

5c33247baf7164d7dec8d24e7143d2b26013a6c4
IvanRamosDataTech <ajaxiank@gmail.com>
2025-05-13 20:17:42 -0600

    - excel file updates: New collumn Ticker Full Name for refresh purposes - updater also differentiates between Development and Production excel data source

--- tracker-poetry/src/tracker_poetry/updater.py
+++ tracker-poetry/src/tracker_poetry/updater.py
@@ -9,13 +9,14 @@ import argparse
 
 ## Definitions
 
-def etl_ppr(connection):
+def etl_ppr(connection, filepath):
+    
     # Verbose
-    ppr_allianz = pd.read_excel("Portafolio Indizado_Patrimonial.xlsx", sheet_name="Allianz", header=None)  
+    ppr_allianz = pd.read_excel(filepath, sheet_name="Allianz", header=None)  
     last_ppr_update = ppr_allianz.iat[0, 1]
     print(f"Last Allianz report  {last_ppr_update}")
     # Extract data from excel
-    ppr_snapshot = pd.read_excel("Portafolio Indizado_Patrimonial.xlsx", sheet_name="Indizado PPR", header=4)   
+    ppr_snapshot = pd.read_excel(filepath, sheet_name="Indizado PPR", header=4)   
     # Transform it
     next_ppr_snapshotID = sqlmanager.next_snapshot_ID(connection, "ppr")
     ppr_snapshot["Snapshot ID"] = next_ppr_snapshotID
@@ -29,13 +30,14 @@ def etl_ppr(connection):
     return ppr_inserted_rows
    
 
-def etl_indexed(connection):
+def etl_indexed(connection, filepath):
     #Verbose
     last_date = sqlmanager.last_update(connection, "indexed")
     print(f"Last Indexed Strategy investment {last_date}")
     # Extract data from excel
-    indexed_snapshot = pd.read_excel("Portafolio Indizado_Patrimonial.xlsx", sheet_name="Indizado FIRE", header=4)
+    indexed_snapshot = pd.read_excel(filepath, sheet_name="Indizado FIRE", header=4)
     # Transform
+    indexed_snapshot.drop(columns=["Ticker Full Name"], inplace=True)
     next_indexed_snapshotID = sqlmanager.next_snapshot_ID(connection, "indexed")
     indexed_snapshot["Snapshot ID"] = next_indexed_snapshotID
     indexed_snapshot["Snapshot Timestamp"] = datetime.now()
@@ -47,13 +49,19 @@ def etl_indexed(connection):
     return indexed_inserted_rows
 
 
-def etl_equity(connection):
+def etl_equity(connection, filepath):
     # TODO Perform equity snapshots
     print(f"TODO: Perform Equity Startegy snapshots \n")
     
     return
 
-
+def get_data_source(environment):
+    if environment == 'Development':
+        return os.getenv('EXCEL_FILE_DEV')
+    elif environment == 'Production':
+        return os.getenv('EXCEL_FILE')
+    else:
+        raise ValueError(f"Unknown environment {environment}")
 
 
 ### Script setup
@@ -91,17 +99,18 @@ print(f"Running Updater ETL process Environment: {args.env} version: {APP_VERSIO
 ## Connect to database
 connection = sqlmanager.connect_to_database(environment=args.env)
 
+excel_local_file = get_data_source(environment=args.env)
 
 if 'All' in args.portfolios:
     print(f"Running All portfolio updates ...\n\n")
-    etl_ppr(connection)
-    etl_indexed(connection)
-    etl_equity(connection)
+    etl_ppr(connection, filepath=excel_local_file)
+    etl_indexed(connection, filepath=excel_local_file)
+    etl_equity(connection, filepath=excel_local_file)
 else:
     if 'PPR' in args.portfolios:
-        etl_ppr(connection)
+        etl_ppr(connection, filepath=excel_local_file)
     if 'Indexed' in args.portfolios:
-        etl_indexed(connection)
+        etl_indexed(connection, filepath=excel_local_file)
     if 'Equity' in args.portfolios:
-        etl_equity(connection)
+        etl_equity(connection, filepath=excel_local_file)
         

f9a0e46d453dd53442fc5c907c64c432c24cdb05
IvanRamosDataTech <ajaxiank@gmail.com>
2025-05-23 12:09:25 -0600

    One Drive sample script that connects to my Azure Application and get my portfolio excel file

--- tracker-poetry/src/tracker_poetry/onedrivedownload.py
+++ tracker-poetry/src/tracker_poetry/onedrivedownload.py
@@ -2,7 +2,9 @@ import msal
 import requests
 import pandas as pd
 
+# Azure App registration 
 CLIENT_ID = "03ad1e3e-524d-4408-aa98-41274583fec8"
+
 TENANT_ID = "consumers"  # For personal Microsoft accounts
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 SCOPES = ["Files.Read"]
@@ -10,7 +12,7 @@ SCOPES = ["Files.Read"]
 def get_token():
     app = msal.PublicClientApplication(CLIENT_ID, authority=AUTHORITY)
     flow = app.initiate_device_flow(scopes=SCOPES)
-    #print(flow["message"])  # Follow the printed instructions to authenticate
+    print(flow["message"])  # Follow the printed instructions to authenticate
     result = app.acquire_token_by_device_flow(flow)
     return result["access_token"]
 
@@ -27,9 +29,10 @@ def main():
     item_path = "/Portfolios/DEV Portafolio Indizado_Patrimonial.xlsx"  # Change to your file's path
     file_bytes = download_file(token, item_path)
     # Read Excel from bytes
-    df = pd.read_excel(pd.io.common.BytesIO(file_bytes), sheet_name="Allianz", header=None)
-    print(df.head())
+    df = pd.read_excel(pd.io.common.BytesIO(file_bytes), sheet_name="Indizado FIRE", header=None)
+    print(df.head(20))
 
 if __name__ == "__main__":
     print("Starting download...")
     main()
+    print("Download completed.")

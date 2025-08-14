9832f1db33bb80451a6f5d352c209f6c9c885229
Ivan Ramos Cortes <ivanramos.datatech@gmail.com>
2025-08-14 17:28:48 -0600

    make enviornment a module containing EnvirnonmentLoader, First test suite running succesfully

--- tracker-poetry/src/tracker_poetry/environment.py
+++ tracker-poetry/src/tracker_poetry/environment.py
@@ -11,6 +11,10 @@ class EnvironmentLoader:
     
     APP_VERSION = "APP_VERSION"
     DB_SERVER = "DB_SERVER"
+    DB_PORT = "DB_PORT"
+    DB_USER = "DB_USER"
+    
+
     AZURE_APP_ID = "AZURE_APP_ID"
 
     @classmethod
@@ -22,3 +26,14 @@ class EnvironmentLoader:
     @staticmethod
     def get(var_name, default=None):
         return os.getenv(var_name, default)
+    
+    @staticmethod
+    def get_db_vars() -> dict:
+        return {
+            "DB_SERVER": EnvironmentLoader.get(EnvironmentLoader.DB_SERVER),
+            "DB_NAME": EnvironmentLoader.get(EnvironmentLoader.DB_NAME),
+            "DB_USER": EnvironmentLoader.get(EnvironmentLoader.DB_USER),
+            "DB_PASSWORD": EnvironmentLoader.get(EnvironmentLoader.DB_PASSWORD),
+        }
+
+    

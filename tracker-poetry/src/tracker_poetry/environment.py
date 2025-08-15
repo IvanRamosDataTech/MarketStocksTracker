5a4d5f906ed44cd7c258c9213e53e0266e3b851c
Ivan Ramos Cortes <ivanramos.datatech@gmail.com>
2025-08-14 19:36:37 -0600

    Environronment encapsultara logica de variables de entorno

--- tracker-poetry/src/tracker_poetry/environment.py
+++ tracker-poetry/src/tracker_poetry/environment.py
@@ -8,32 +8,42 @@ class EnvironmentLoader:
     Ensures variables are available for all scripts.
     """
     _loaded = False
-    
-    APP_VERSION = "APP_VERSION"
-    DB_SERVER = "DB_SERVER"
-    DB_PORT = "DB_PORT"
-    DB_USER = "DB_USER"
-    
-
-    AZURE_APP_ID = "AZURE_APP_ID"
+    _env = None
 
     @classmethod
-    def load(cls, dotenv_path=".env"):
+    def load(cls, env, dotenv_path=".env"):
+        if env not in ["Development", "Production"]:
+            raise ValueError("Environment must be either 'Development' or 'Production'.")
+        cls._env = env
         if not cls._loaded:
             load_dotenv(dotenv_path=Path(dotenv_path))
             cls._loaded = True
 
+    @classmethod
+    def clear(cls):
+        cls._loaded = False
+        cls._env = None
+
     @staticmethod
-    def get(var_name, default=None):
-        return os.getenv(var_name, default)
+    def _get(var_name, default=None):
+        return os.getenv(f"{var_name}_DEV" if EnvironmentLoader._env == "Development" else var_name, default)
+
+    @staticmethod
+    def get_app_version() -> str:
+        return EnvironmentLoader._get("APP_VERSION", "1.0.0")
     
     @staticmethod
     def get_db_vars() -> dict:
         return {
-            "DB_SERVER": EnvironmentLoader.get(EnvironmentLoader.DB_SERVER),
-            "DB_NAME": EnvironmentLoader.get(EnvironmentLoader.DB_NAME),
-            "DB_USER": EnvironmentLoader.get(EnvironmentLoader.DB_USER),
-            "DB_PASSWORD": EnvironmentLoader.get(EnvironmentLoader.DB_PASSWORD),
+            "DB_SERVER": EnvironmentLoader._get("DB_SERVER"),
+            "DB_NAME": EnvironmentLoader._get("DB_NAME"),
+            "DB_USER": EnvironmentLoader._get("DB_USER"),
+            "DB_PASSWORD": EnvironmentLoader._get("DB_PASSWORD"),
+            "PORT": EnvironmentLoader._get("DB_PORT")
         }
+    
+    @staticmethod
+    def get_azure_app_id():
+        return EnvironmentLoader._get("AZURE_APP_ID")
 
     
--- tracker-poetry/tests/test_environment_loader.py
+++ tracker-poetry/tests/test_environment_loader.py
@@ -2,18 +2,61 @@ import os
 import pytest
 from tracker_poetry.environment import EnvironmentLoader
 
-@pytest.fixture(autouse=True)
-def set_env_vars(monkeypatch):
-    monkeypatch.setenv(EnvironmentLoader.APP_VERSION, "1.0.4v")
-    monkeypatch.setenv(EnvironmentLoader.DB_SERVER, "localhost")
-    monkeypatch.setenv(EnvironmentLoader.AZURE_APP_ID, "80xb-1603-4bf229")
+@pytest.fixture
+def env_vars(monkeypatch):
+    monkeypatch.setenv("APP_VERSION", "1.0.4v")
+    monkeypatch.setenv("APP_VERSION_DEV", "0.56.1beta")
+    monkeypatch.setenv("DB_SERVER", "db.supabase.urgttyz889")
+    monkeypatch.setenv("DB_PORT", "5432")
+    monkeypatch.setenv("DB_NAME", "prod_db")
+    monkeypatch.setenv("DB_USER", "prod_user")
+    monkeypatch.setenv("DB_PASSWORD", "prod_password")
+    monkeypatch.setenv("DB_SERVER_DEV", "localhost")
+    monkeypatch.setenv("DB_PORT_DEV", "1123")
+    monkeypatch.setenv("DB_NAME_DEV", "test_db")
+    monkeypatch.setenv("DB_USER_DEV", "test_user")
+    monkeypatch.setenv("DB_PASSWORD_DEV", "test_password")
+    monkeypatch.setenv("AZURE_APP_ID", "80xb-1603-4bf229")
+    monkeypatch.setenv("AZURE_APP_ID_DEV", "1111-2222-wweplt")
     EnvironmentLoader._loaded = True  # Prevents loading .env
 
-def test_get_app_version():
-    assert EnvironmentLoader.get(EnvironmentLoader.APP_VERSION) == "1.0.4v"
+@pytest.fixture
+def dev_loader():
+    EnvironmentLoader.load("Development")
+    return EnvironmentLoader
+    
+@pytest.fixture
+def prod_loader():
+    EnvironmentLoader.load("Production")
+    return EnvironmentLoader
 
-def test_get_db_server():
-    assert EnvironmentLoader.get(EnvironmentLoader.DB_SERVER) == "localhost"
+def test_get_app_version(dev_loader, env_vars):
+    assert EnvironmentLoader.get_app_version() == "0.56.1beta"
+    
+def test_get_app_version(prod_loader, env_vars):
+    assert EnvironmentLoader.get_app_version() == "1.0.4v"
 
-def test_get_azure_app_id():
-    assert EnvironmentLoader.get(EnvironmentLoader.AZURE_APP_ID) == "80xb-1603-4bf229"
+def test_get_dev_db_vars(dev_loader, env_vars):
+    db_vars = EnvironmentLoader.get_db_vars()
+    assert db_vars["DB_SERVER"] == "localhost"
+    assert db_vars["PORT"] == "1123"
+    assert db_vars["DB_NAME"] == "test_db"
+    assert db_vars["DB_USER"] == "test_user"
+    assert db_vars["DB_PASSWORD"] == "test_password"
+
+def test_get_prod_db_vars(prod_loader, env_vars):
+    db_vars = EnvironmentLoader.get_db_vars()
+    assert db_vars["DB_SERVER"] == "db.supabase.urgttyz889"
+    assert db_vars["PORT"] == "5432"
+    assert db_vars["DB_NAME"] == "prod_db"
+    assert db_vars["DB_USER"] == "prod_user"
+    assert db_vars["DB_PASSWORD"] == "prod_password"
+
+
+def test_get_azure_ap_id(prod_loader, env_vars):
+    assert EnvironmentLoader.get_azure_app_id() == "80xb-1603-4bf229"
+
+def test_get_azure_ap_id_dev(dev_loader, env_vars):
+    assert EnvironmentLoader.get_azure_app_id() == "1111-2222-wweplt"
+
+
